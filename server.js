8e9474f496663745810223feae7600046c8c7843
drerudin <drerudin@gmail.com>
2022-06-29 18:37:39 -0700

    larry

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming json data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
